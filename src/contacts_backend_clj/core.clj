b40c152c152c7b5c1a755dd897d2e14aabfecb86
vinay427 <vhiremath95@gmail.com>
2015-07-22 20:58:44 -0400

    Added CORS allow all header

--- src/contacts_backend_clj/core.clj
+++ src/contacts_backend_clj/core.clj
@@ -2,7 +2,8 @@
   (:require [compojure.core :refer :all]
             [org.httpkit.server :refer [run-server]]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
-            [contacts-backend-clj.db :as db]))
+            [contacts-backend-clj.db :as db]
+            [ring.middleware.cors :refer [wrap-cors]]))
 
 (defroutes contacts-app
   (GET "/contacts" []
@@ -13,9 +14,13 @@
             :body
             db/add-item)))
 
+(defn handler
+  [routes]
+  (wrap-cors (wrap-json-response (wrap-json-body contacts-app {:keywords? true}))
+             :access-control-allow-methods [:get :post]))
+
 (defn -main
   []
   (do (db/create-table)
       (println "Running...")
-      (run-server (wrap-json-response (wrap-json-body contacts-app {:keywords? true}))
-                  {:port (read-string (System/getenv "PORT"))})))
+      (run-server (handler) {:port (read-string (System/getenv "PORT"))})))
